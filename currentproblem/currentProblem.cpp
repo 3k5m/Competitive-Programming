d4f5a61a64a90a62c47efccaa132dcc22f2a2b2e
3k5m <96869224+3k5m@users.noreply.github.com>
2024-02-23 10:00:16 +0900

    gitignore

--- currentproblem/currentProblem.cpp
+++ currentproblem/currentProblem.cpp
@@ -1,36 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-    string x;
-    int sum=0,loops=0, i=0;
-    int num;
-    bool neg = false;
-
-int parsenum(){
-    neg = false;
-    if(x[i]=='-'){
-        neg = true;
-        i++;
-    }
-    num = x[i]-'0';
-    i++;
-    while(x[i]>='0' && x[i]<='9'){
-        num*=10;
-        num+=x[i]-'0';
-        i++;
-    }
-    if(neg) num*=-1;
-    return num;
-}
-
 void solve(){
-    getline(cin, x);
-    for(i=0;i<x.length();i++){
-        if((x[i]>='0' && x[i]<='9') || x[i]=='-'){
-            sum+=parsenum();
-        }
-    }
-    cout<<sum;
+    
 }
 
 int main() {
